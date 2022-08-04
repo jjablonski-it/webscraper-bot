c71ce430480ac15098d0b017d07dc86fbbec346c
Jakub Jablonski <jjablonski.it@gmail.com>
2022-08-04 22:40:44 +0200

    scraper wip

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import { Scraper } from './scraper.js'
 import { config } from 'dotenv'
 config()
 
-const SCRAPE_URL = ''
-const scraper = new Scraper(SCRAPE_URL)
+const URL =
+  'https://www.olx.pl/d/nieruchomosci/mieszkania/wynajem/gdansk/q-mieszkanie/?search%5Bfilter_float_price%3Afrom%5D=1000&search%5Bfilter_float_price%3Ato%5D=3300&search%5Bfilter_enum_furniture%5D%5B0%5D=yes&search%5Bfilter_float_m%3Afrom%5D=25&search%5Bfilter_enum_rooms%5D%5B0%5D=two&reason=observed_search'
+const scraper = new Scraper(URL)
+const dom = await scraper.getDom()
+const x = dom.window.document.querySelectorAll('[data-cy=l-card]')
+console.log(x)
+// const dcClient = new Discord({
+//   token: process.env.CLIENT_TOKEN,
+//   channelId: '1004810730761093152',
+// })
+// await dcClient.login()
 
-const dcClient = new Discord({
-  token: process.env.CLIENT_TOKEN,
-  channelId: '1004810730761093152',
-})
-await dcClient.login()
+const cards = await scraper.getCards()
+const json = JSON.stringify(cards)
+
+console.log(json);
+
+// console.log(JSON.stringify(cards[0].asElement()))
---
+++ src/scraper.js
@@ -0,0 +1,31 @@
+import { JSDOM } from 'jsdom'
+import axios from 'axios'
+import puppeteer from 'puppeteer'
+
+export class Scraper {
+  constructor(url) {
+    this.url = url
+  }
+
+  async getDom(url) {
+    const data = await axios.get(url || this.url).then((res) => res.data)
+    const dom = new JSDOM(data)
+    return dom
+  }
+
+  async getCards(url) {
+    const cardSlector = '[data-cy=l-card]'
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+    const page = await browser.newPage()
+    await page.goto(url || this.url)
+    await page.waitForSelector(cardSlector)
+    const links = await page.$$(`${cardSlector} > a`)
+    const res = await Promise.all(links.map((handle) => handle.getProperty('href')))
+    const hrefs = await Promise.all(res.map((href) => href.jsonValue()))
+    await page.close()
+    return 
+  }
+}
