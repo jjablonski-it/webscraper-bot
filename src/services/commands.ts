22ea674be157dc276ee68651571bf2d46dd1aa2c
Jakub Jablonski <jjablonski.it@gmail.com>
2022-08-12 00:40:24 +0200

    add commands

--- src/services/commands.ts
+++ src/services/commands.ts
@@ -1,72 +1,90 @@
 import { Job } from '@prisma/client'
-import { REST, Routes, SlashCommandBuilder } from 'discord.js'
+import {
+  REST,
+  Routes,
+  SlashCommandBuilder,
+  SlashCommandStringOption,
+} from 'discord.js'
 import { CONFIG } from '../config'
 import { getJobs } from './db'
 
-const getCommands = (jobs: Job[]) => [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-  new SlashCommandBuilder()
-    .setName('create-job')
-    .setDescription('Creates a new scraper job')
-    .addStringOption((option) =>
-      option.setName('name').setDescription('Name of the job').setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName('url')
-        .setDescription('The URL to scrape')
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName('selector')
-        .setDescription('The query selector to a link')
-        .setRequired(true)
-    )
-    .addIntegerOption((option) =>
-      option
-        .setName('interval')
-        .setDescription('The interval to scrape in minutes')
-        .setMinValue(0)
-        .setMaxValue(1_440)
-        .setRequired(true)
-    )
-    .addChannelOption((option) =>
-      option
-        .setName('channel')
-        .setDescription('The channel to send the links to')
-    ),
-  new SlashCommandBuilder()
-    .setName('run-job')
-    .setDescription('Runs a scraper job')
-    .addStringOption((option) =>
-      option
-        .setName('name')
-        .setDescription('Name of the job')
-        .setRequired(true)
-        .addChoices(...jobs.map((job) => ({ name: job.name, value: job.name })))
-    ),
-  new SlashCommandBuilder()
-    .setName('list-jobs')
-    .setDescription('Lists all scraper jobs'),
-  // new SlashCommandBuilder()
-  // .setName('stop-job')
-  // .setDescription('Stops a scraper job')
-  // .addStringOption((option) =>
-  //   option.setName('name').setDescription('Name of the job').setRequired(true)
-  // )
-  // .addChannelOption((option) =>
-  //   option
-  //     .setName('channel')
-  //     .setDescription('The channel to send the links to')
-  // ),
-  // new SlashCommandBuilder()
-  // .setName('delete-job')
-  // .setDescription('Deletes a scraper job')
-]
+// type CommandName =
+//   | 'ping'
+//   | 'create-job'
+//   | 'delete-job'
+//   | 'disable-job'
+//   | 'enable-job'
+//   | 'run-job'
+//   | 'list-jobs'
+
+const getCommands = (jobs: Job[]) => {
+  const existingJobOption = (option: SlashCommandStringOption) =>
+    option
+      .setName('name')
+      .setDescription('Name of the job')
+      .setRequired(true)
+      .addChoices(...jobs.map((job) => ({ name: job.name, value: job.name })))
+
+  return [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    },
+    new SlashCommandBuilder()
+      .setName('create-job')
+      .setDescription('Creates a new scraper job')
+      .addStringOption((option) =>
+        option
+          .setName('name')
+          .setDescription('Name of the job')
+          .setRequired(true)
+      )
+      .addStringOption((option) =>
+        option
+          .setName('url')
+          .setDescription('The URL to scrape')
+          .setRequired(true)
+      )
+      .addStringOption((option) =>
+        option
+          .setName('selector')
+          .setDescription('The query selector to a link')
+          .setRequired(true)
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName('interval')
+          .setDescription('The interval to scrape in minutes')
+          .setMinValue(0)
+          .setMaxValue(1_440)
+          .setRequired(true)
+      )
+      .addChannelOption((option) =>
+        option
+          .setName('channel')
+          .setDescription('The channel to send the links to')
+      ),
+    new SlashCommandBuilder()
+      .setName('delete-job')
+      .setDescription('Deletes a scraper job')
+      .addStringOption(existingJobOption),
+    new SlashCommandBuilder()
+      .setName('disable-job')
+      .setDescription('Disables a scraper job')
+      .addStringOption(existingJobOption),
+    new SlashCommandBuilder()
+      .setName('enable-job')
+      .setDescription('Enables a scraper job')
+      .addStringOption(existingJobOption),
+    new SlashCommandBuilder()
+      .setName('run-job')
+      .setDescription('Runs a scraper job')
+      .addStringOption(existingJobOption),
+    new SlashCommandBuilder()
+      .setName('list-jobs')
+      .setDescription('Lists all scraper jobs'),
+  ]
+}
 
 export const registerCommands = async (clientId: string, guildId: string) => {
   // TODO add parameters per guild
