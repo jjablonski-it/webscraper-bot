52a997a85ab25669d6f72c4910eb3cfda7f619ad
Jakub Jablonski <jjablonski.it@gmail.com>
2022-08-11 22:51:45 +0200

    add run job

--- src/services/jobs.ts
+++ src/services/jobs.ts
@@ -10,22 +10,27 @@ export const createJob = async (jobInput: Prisma.JobCreateInput) => {
   jobs.push(createdJob)
 }
 
-export const runJobs = async () => {
-  for (const { name, guildId, active, url, selector } of jobs) {
-    try {
-      console.log(`Running job ${name} from ${guildId}`)
-      if (!active) continue
-      const links = await scrapeLinks(url, selector)
-      const existingLinks = await getExistingLinks(guildId, name)
-      const newLinks = links.filter((link) => !existingLinks.includes(link))
-      if (newLinks.length) {
-        await saveLinks(guildId, name, newLinks)
-        console.log(`Found ${newLinks.length} new links`)
-        console.log(newLinks)
-        await sendMessage(name, newLinks.join('\n'))
-      }
-    } catch (e) {
-      console.error(`Error running job ${name} from ${guildId}`, e)
+export const runJob = async (job: Job) => {
+  const { name, guildId, url, selector, channelId } = job
+  try {
+    const links = await scrapeLinks(url, selector)
+    const existingLinks = await getExistingLinks(guildId, name)
+    const newLinks = links.filter((link) => !existingLinks.includes(link))
+    if (newLinks.length) {
+      await saveLinks(guildId, name, newLinks)
+      console.log(`Found ${newLinks.length} new links`)
+      console.log(newLinks)
+      await sendMessage(channelId, newLinks.join('\n'))
     }
+  } catch (e) {
+    console.error(`Error running job ${name} from ${guildId}`, e)
+  }
+}
+
+export const runJobs = async () => {
+  for (const job of jobs) {
+    console.log(`Running job ${job.name} from ${job.guildId}`)
+    if (!job.active) continue
+    await runJob(job)
   }
 }
