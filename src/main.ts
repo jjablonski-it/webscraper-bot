7df877febbe07883664e1db02d5bce5dae15ba18
Jakub Jablonski <jjablonski.it@gmail.com>
2022-08-13 11:41:32 +0200

    cleanup main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { TextChannel } from 'discord.js'
 import { CONFIG } from './config.js'
 import { getClient } from './controllers/discord.js'
 import { runWebServer } from './controllers/web.js'
@@ -10,14 +9,11 @@ const client = await getClient()
 const existingGuilds = await getGuilds()
 
 client.guilds.cache.forEach((guild) => {
-  if (!existingGuilds.some((g) => g.id === guild.id)) {
+  const guildExists = existingGuilds.some((g) => g.id === guild.id)
+  if (!guildExists) {
     saveGuild(guild.id)
   }
   registerCommands(CONFIG.CLIENT_ID, guild.id)
-  const channel = guild.channels.cache.find(
-    (channel) => channel.name === 'bot'
-  ) as TextChannel
-  channel?.send('Test')
 })
 
 runWebServer()
