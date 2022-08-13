278f4b559ded8b9317e56011c8d376e676405d94
Jakub Jablonski <jjablonski.it@gmail.com>
2022-08-13 13:45:34 +0200

    remove console log

--- src/services/commands.ts
+++ src/services/commands.ts
@@ -111,7 +111,6 @@ const getCommands = (jobs: Job[]) => [
 ]
 
 export const registerCommands = async (clientId: string, guildId: string) => {
-  // TODO add parameters per guild
   const rest = new REST({ version: '10' }).setToken(CONFIG.CLIENT_TOKEN)
   try {
     console.log(`Registering commands for ${guildId}`)
