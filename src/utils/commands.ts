937572e07520bd4038db5ae2fd01071ef981e25d
Jakub Jablonski <jjablonski.it@gmail.com>
2022-11-05 18:41:22 +0100

    adjust intervals

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,10 +1,5 @@
 import { Job } from '@prisma/client'
-import {
-  REST,
-  Routes,
-  SlashCommandBuilder,
-  SlashCommandStringOption,
-} from 'discord.js'
+import { REST, Routes, SlashCommandBuilder, SlashCommandStringOption } from 'discord.js'
 import { CONFIG } from '../config.js'
 import { getJobs } from '../services/db.js'
 
@@ -32,20 +27,10 @@ const getCommands = (jobs: Job[]) => [
   new SlashCommandBuilder()
     .setName('create-job')
     .setDescription('Creates a new scraper job')
+    .addStringOption((option) => option.setName('name').setDescription('Name of the job').setRequired(true))
+    .addStringOption((option) => option.setName('url').setDescription('The URL to scrape').setRequired(true))
     .addStringOption((option) =>
-      option.setName('name').setDescription('Name of the job').setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName('url')
-        .setDescription('The URL to scrape')
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName('selector')
-        .setDescription('The query selector to a link')
-        .setRequired(true)
+      option.setName('selector').setDescription('The query selector to a link').setRequired(true)
     )
     .addIntegerOption((option) =>
       option
@@ -55,11 +40,12 @@ const getCommands = (jobs: Job[]) => [
         .setMaxValue(1_440)
         .addChoices(
           ...[
-            { name: '1 minute', value: 1 },
             { name: '5 minutes', value: 5 },
             { name: '10 minutes', value: 10 },
+            { name: '15 minutes', value: 15 },
             { name: '30 minutes', value: 30 },
             { name: '1 hour', value: 60 },
+            { name: '2 hours', value: 120 },
             { name: '6 hours', value: 360 },
             { name: '12 hours', value: 720 },
             { name: '1 day', value: 1_440 },
@@ -68,10 +54,7 @@ const getCommands = (jobs: Job[]) => [
         .setRequired(true)
     )
     .addBooleanOption((option) =>
-      option
-        .setName('active')
-        .setDescription('Whether the job is active')
-        .setRequired(false)
+      option.setName('active').setDescription('Whether the job is active').setRequired(false)
     )
     .addBooleanOption((option) =>
       option
@@ -79,11 +62,7 @@ const getCommands = (jobs: Job[]) => [
         .setDescription('Whether the query params should be removed from the url')
         .setRequired(false)
     )
-    .addChannelOption((option) =>
-      option
-        .setName('channel')
-        .setDescription('The channel to send the links to')
-    ),
+    .addChannelOption((option) => option.setName('channel').setDescription('The channel to send the links to')),
   new SlashCommandBuilder()
     .setName('delete-job')
     .setDescription('Deletes a scraper job')
@@ -100,24 +79,14 @@ const getCommands = (jobs: Job[]) => [
     .setName('run-job')
     .setDescription('Runs a scraper job')
     .addStringOption(existingJobOption(jobs)),
-  new SlashCommandBuilder()
-    .setName('list-jobs')
-    .setDescription('Lists all scraper jobs'),
+  new SlashCommandBuilder().setName('list-jobs').setDescription('Lists all scraper jobs'),
   new SlashCommandBuilder()
     .setName('update-job')
     .setDescription('Edits a scraper job')
     .addStringOption(existingJobOption(jobs))
+    .addStringOption((option) => option.setName('url').setDescription('The URL to scrape').setRequired(false))
     .addStringOption((option) =>
-      option
-        .setName('url')
-        .setDescription('The URL to scrape')
-        .setRequired(false)
-    )
-    .addStringOption((option) =>
-      option
-        .setName('selector')
-        .setDescription('The query selector to a link')
-        .setRequired(false)
+      option.setName('selector').setDescription('The query selector to a link').setRequired(false)
     )
     .addIntegerOption((option) =>
       option
@@ -146,19 +115,10 @@ const getCommands = (jobs: Job[]) => [
         .setRequired(false)
     )
     .addBooleanOption((option) =>
-      option
-        .setName('active')
-        .setDescription('Whether the job is active')
-        .setRequired(false)
+      option.setName('active').setDescription('Whether the job is active').setRequired(false)
     )
-    .addChannelOption((option) =>
-      option
-        .setName('channel')
-        .setDescription('The channel to send the links to')
-    ),
-  new SlashCommandBuilder()
-    .setName('stats')
-    .setDescription('Shows some stats about the bot'),
+    .addChannelOption((option) => option.setName('channel').setDescription('The channel to send the links to')),
+  new SlashCommandBuilder().setName('stats').setDescription('Shows some stats about the bot'),
 ]
 
 export const registerCommands = async (clientId: string, guildId: string) => {
