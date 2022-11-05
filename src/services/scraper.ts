ff1974a6013053a749cae57514283d2dfaf4e1be
Jakub Jablonski <jjablonski.it@gmail.com>
2022-11-05 18:37:21 +0100

    add shorter timeout

--- src/services/scraper.ts
+++ src/services/scraper.ts
@@ -9,12 +9,10 @@ export const scrapeLinks = async (url: string, selector: string): Promise<Array<
   const page = await browser.newPage()
   await page.setCacheEnabled(false)
   await page.goto(url)
-  await page.waitForSelector(selector)
+  await page.waitForSelector(selector, { timeout: 5000 })
   const links = await page.$$(selector)
-  const res = await Promise.all(
-    links.map((handle) => handle.getProperty('href'))
-  )
-  const hrefs = await Promise.all(res.map((href) => href.jsonValue())) as string[]
+  const res = await Promise.all(links.map((handle) => handle.getProperty('href')))
+  const hrefs = (await Promise.all(res.map((href) => href.jsonValue()))) as string[]
   await page.close()
   return hrefs
 }
